44f825550933a11bc15a3e33235b3e1400749b48
laraholand <laraholand0921@gmail.com>
2026-01-04 20:44:17 +0600

    fix a issue

--- lib/pages/authentication_page.dart
+++ lib/pages/authentication_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:itemrdc/util/glow_text_field.dart';
 
+/// Sign Up Page
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
@@ -15,7 +16,8 @@ class _SignUpPageState extends State<SignUpPage>
   final passwordController = TextEditingController();
 
   late AnimationController animationController;
-  late Animation<double> animation;
+  late Animation<double> widthAnimation;
+  late Animation<double> heightAnimation;
 
   @override
   void initState() {
@@ -23,22 +25,23 @@ class _SignUpPageState extends State<SignUpPage>
 
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 5),
+      duration: const Duration(milliseconds: 800),
     );
 
-    // প্রথমে ডামি Tween দিয়ে রাখি
-    animation = Tween<double>(begin: 1, end: 220).animate(animationController);
+    // ডামি Tween
+    widthAnimation = Tween<double>(begin: 1, end: 220).animate(animationController);
+    heightAnimation = Tween<double>(begin: 1, end: 220).animate(animationController);
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      // স্ক্রিন সাইজ পাওয়া
-      final screenWidth = MediaQuery.of(context).size.width;
+      final screenSize = MediaQuery.of(context).size;
 
-      // Tween আপডেট করি পুরো স্ক্রিন থেকে ছোট 220 পর্যন্ত
-      animation = Tween<double>(begin: screenWidth, end: 220).animate(
+      widthAnimation = Tween<double>(begin: screenSize.width, end: 220).animate(
         CurvedAnimation(parent: animationController, curve: Curves.easeOut),
-      )..addListener(() {
-          setState(() {});
-        });
+      )..addListener(() => setState(() {}));
+
+      heightAnimation = Tween<double>(begin: screenSize.height, end: 220).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      );
 
       animationController.forward();
     });
@@ -106,6 +109,7 @@ class _SignUpPageState extends State<SignUpPage>
               ],
             ),
           ),
+
           Positioned(
             bottom: 20,
             left: 20,
@@ -120,7 +124,6 @@ class _SignUpPageState extends State<SignUpPage>
             ),
           ),
 
-          /// Bottom Right
           Positioned(
             bottom: 15,
             right: 20,
@@ -135,12 +138,156 @@ class _SignUpPageState extends State<SignUpPage>
           ClipPath(
             clipper: QuarterCircleClipper(),
             child: Container(
-              width: animation.value,
-              height: animation.value,
+              width: widthAnimation.value,
+              height: heightAnimation.value,
               color: const Color(0xffcff3f4),
               child: const Center(
                 child: Text(
                   "Welcome In\nItem RDC",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// Login Page
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  late AnimationController animationController;
+  late Animation<double> widthAnimation;
+  late Animation<double> heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    widthAnimation = Tween<double>(begin: 1, end: 220).animate(animationController);
+    heightAnimation = Tween<double>(begin: 1, end: 220).animate(animationController);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final screenSize = MediaQuery.of(context).size;
+
+      widthAnimation = Tween<double>(begin: screenSize.width, end: 220).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      )..addListener(() => setState(() {}));
+
+      heightAnimation = Tween<double>(begin: screenSize.height, end: 220).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      );
+
+      animationController.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(20, 240, 20, 100),
+            child: Column(
+              children: [
+                GlowTextField(
+                  label: "Email address",
+                  controller: emailController,
+                  backgroundColor: Colors.white,
+                  textColor: Colors.black,
+                ),
+                const SizedBox(height: 15),
+                GlowTextField(
+                  label: "Password",
+                  controller: passwordController,
+                  isPassword: true,
+                  textColor: Colors.black,
+                ),
+                const SizedBox(height: 25),
+                Row(
+                  children: [
+                    const Text(
+                      "Sign In",
+                      style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_forward, color: Colors.white),
+                      style: IconButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: const CircleBorder(),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Don't have an account?"),
+            ),
+          ),
+
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: InkWell(
+              onTap: () {
+                debugPrint("Forget password clicked");
+              },
+              child: const Text("Forget password"),
+            ),
+          ),
+
+          /// Animated ClipPath
+          ClipPath(
+            clipper: QuarterCircleClipper(),
+            child: Container(
+              width: widthAnimation.value,
+              height: heightAnimation.value,
+              color: const Color(0xffcff3f4),
+              child: const Center(
+                child: Text(
+                  "Welcome\nBack",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 30,
@@ -161,164 +308,17 @@ class QuarterCircleClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     final path = Path();
-
-    path.moveTo(size.width, 0); // M width 0
+    path.moveTo(size.width, 0);
     path.arcToPoint(
-      Offset(0, size.height), // 0 height
+      Offset(0, size.height),
       radius: Radius.circular(size.width),
       clockwise: true,
     );
     path.lineTo(0, 0);
     path.close();
-
     return path;
   }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
-
-/// Login Page
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  late AnimationController animationController;
-  late Animation animation;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    );
-
-     animation = Tween<double>(begin: 1, end: 220).animate(animationController);
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      // স্ক্রিন সাইজ পাওয়া
-      final screenWidth = MediaQuery.of(context).size.width;
-
-      // Tween আপডেট করি পুরো স্ক্রিন থেকে ছোট 220 পর্যন্ত
-      animation = Tween<double>(begin: screenWidth, end: 220).animate(
-        CurvedAnimation(parent: animationController, curve: Curves.easeOut),
-      )..addListener(() {
-          setState(() {});
-        });
-
-      animationController.forward();
-    });
-
-    
-    
-  }
-    
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Stack(
-        children: [
-
-          /// Scrollable Form
-          SingleChildScrollView(
-            padding: const EdgeInsets.fromLTRB(20, 240, 20, 100),
-            child: Column(
-              children: [
-                GlowTextField(
-                  label: "Email address",
-                  controller: emailController,
-                  backgroundColor: Colors.white,
-                  textColor: Colors.black,
-                ),
-                const SizedBox(height: 15),
-                GlowTextField(
-                  label: "Password",
-                  controller: passwordController,
-                  isPassword: true,
-                  textColor: Colors.black,
-                ),
-                const SizedBox(height: 25),
-
-                Row(
-                  children: [
-                    const Text(
-                      "Sign In",
-                      style: TextStyle(
-                        fontSize: 27,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      onPressed: () {
-                        debugPrint("Login clicked");
-                      },
-                      tooltip: "Login",
-                      icon: const Icon(Icons.arrow_forward, color: Colors.white),
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        shape: const CircleBorder(),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          /// Bottom Left
-          Positioned(
-            bottom: 20,
-            left: 20,
-            child: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: const Text("Don't have an account?"),
-            ),
-          ),
-
-          /// Bottom Right
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: InkWell(
-              onTap: () {
-                debugPrint("Forget password clicked");
-              },
-              child: const Text("Forget password"),
-            ),
-          ),
-                    /// Header Shape
-          ClipPath(
-            clipper: QuarterCircleClipper(),
-            child: Container(
-              width: animation.value==1.0?500:animation.value,
-              height: animation.value==1.0?500:animation.value,
-              color: const Color(0xffcff3f4),
-              child: const Center(
-                child: Text(
-                  "Welcome\nBack",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-        ],
-      ),
-    );
-  }
 }

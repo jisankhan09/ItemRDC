2b0cba5bb21bd9f0332f397fdae614f82291efd2
laraholand <laraholand0921@gmail.com>
2026-01-05 08:30:35 +0600

    Auto commit from automation tool

--- lib/pages/authentication_page.dart
+++ lib/pages/authentication_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:itemrdc/util/glow_text_field.dart';
+import 'package:itemrdc/util/liquid_button.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class _SignUpPageState extends State<SignUpPage> {
   TextEditingController phoneController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +37,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   child: GlowTextField(
                     label: "Enter Email",
                     backgroundColor: Colors.white,
-                    controller: passwordController,
+                    controller: emailController,
                     textColor: Colors.black,
                   ),
                 ),
@@ -48,57 +50,65 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                 ),
                 Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, top: 20),
-                    child: Row(
-                      children: [
-                        const Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            fontSize: 27,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
+                  padding:
+                      const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {
-                            print("Sign Up clicked");
-                          },
-                          icon: const Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                          ),
-                          style: IconButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            shape: const CircleBorder(),
-                          ),
+                      ),
+                      const Spacer(),
+                      LiquidButton(
+                        width: 55,
+                        height: 55,
+                        onTap: () {
+                          print("Sign Up clicked");
+                        },
+                        backgroundColor: Colors.black.withOpacity(0.8),
+                        borderColor: Colors.black,
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
                         ),
-                      ],
-                    )),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                LiquidButton(
+                  width: 220,
+                  height: 40,
+                  backgroundColor: Colors.white.withOpacity(0.6),
+                  borderColor: Colors.black26,
+                  onTap: () {
+                    print("Already have an account clicked");
+                  },
+                  child: const Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
               ],
             ),
           ),
           Positioned(
-            child: InkWell(
-              child: const Text("Already have an account?"),
-              onTap: () {
-                print(" clicked");
-              },
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.help),
             ),
-            bottom: 20,
-            left: 20,
-          ),
-          Positioned(
-            child: IconButton(onPressed: () {}, icon: const Icon(Icons.help)),
             bottom: 13,
             right: 20,
           ),
           ClipPath(
             clipper: QuarterCircleClipper(),
             child: Container(
-              child: Center(
-                child: const Text(
+              child: const Center(
+                child: Text(
                   "Welcome In \nItem RDC",
                   style: TextStyle(
                       fontSize: 30,
@@ -117,29 +127,7 @@ class _SignUpPageState extends State<SignUpPage> {
   }
 }
 
-class QuarterCircleClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-
-    path.moveTo(size.width * 1.0, 0); // M80 0
-
-    path.arcToPoint(
-      Offset(0, size.height * 1.0), // 0 80
-      radius: Radius.circular(size.width * 1.0), // 80 80
-      clockwise: true,
-    );
-
-    path.lineTo(0, 0); // L0 0
-    path.close(); // Z
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
-
+// ------------------------- Login Page -------------------------
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -148,6 +136,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -156,8 +147,8 @@ class _LoginState extends State<Login> {
           ClipPath(
             clipper: QuarterCircleClipper(),
             child: Container(
-              child: Center(
-                child: const Text(
+              child: const Center(
+                child: Text(
                   "Welcome \nBack",
                   style: TextStyle(
                       fontSize: 30,
@@ -179,6 +170,7 @@ class _LoginState extends State<Login> {
                   child: GlowTextField(
                     label: "Enter Email",
                     backgroundColor: Colors.white,
+                    controller: emailController,
                     textColor: Colors.black,
                   ),
                 ),
@@ -186,63 +178,99 @@ class _LoginState extends State<Login> {
                   padding: const EdgeInsets.all(10),
                   child: GlowTextField(
                     label: "Enter Password",
+                    controller: passwordController,
                     textColor: Colors.black,
                   ),
                 ),
                 Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, top: 20),
-                    child: Row(
-                      children: [
-                        const Text(
-                          "Sign In",
-                          style: TextStyle(
-                            fontSize: 27,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
+                  padding:
+                      const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {
-                            print("Login clicked");
-                          },
-                          icon: const Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                          ),
-                          style: IconButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            shape: const CircleBorder(),
-                          ),
+                      ),
+                      const Spacer(),
+                      LiquidButton(
+                        width: 55,
+                        height: 55,
+                        onTap: () {
+                          print("Login clicked");
+                        },
+                        backgroundColor: Colors.black.withOpacity(0.85),
+                        borderColor: Colors.black,
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
                         ),
-                      ],
-                    )),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LiquidButton(
+                      width: 240,
+                      height: 40,
+                      backgroundColor: Colors.white.withOpacity(0.6),
+                      borderColor: Colors.black26,
+                      onTap: () {
+                        print("Go to Sign Up");
+                      },
+                      child: const Text(
+                        "Don't have any account?",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    LiquidButton(
+                      width: 150,
+                      height: 38,
+                      backgroundColor: Colors.white.withOpacity(0.6),
+                      borderColor: Colors.black26,
+                      onTap: () {
+                        print("Forget password clicked");
+                      },
+                      child: const Text(
+                        "Forget password",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
-          ),
-          Positioned(
-            child: InkWell(
-              child: const Text("Don't have any account?"),
-              onTap: () {
-                print("Forget password clicked");
-              },
-            ),
-            bottom: 20,
-            left: 20,
-          ),
-          Positioned(
-            child: InkWell(
-              child: const Text("Forget password"),
-              onTap: () {
-                print("Forget password clicked");
-              },
-            ),
-            bottom: 20,
-            right: 20,
           ),
         ],
       ),
     );
   }
+}
+class QuarterCircleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+
+    path.moveTo(size.width * 1.0, 0);
+
+    path.arcToPoint(
+      Offset(0, size.height * 1.0),
+      radius: Radius.circular(size.width * 1.0),
+      clockwise: true,
+    );
+
+    path.lineTo(0, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
